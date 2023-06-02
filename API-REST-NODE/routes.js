6c0ac3cb83d8e124d2b840b1dcc22ab313cad409
FrankBedoyac4 <franksurenuga@gmail.com>
2023-06-02 11:39:19 -0500

    Added a new get to get a one book

--- API-REST-NODE/routes.js
+++ API-REST-NODE/routes.js
@@ -14,6 +14,22 @@ routes.get("/", (req, res) => {
   });
 });
 
+routes.get("/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+
+    conn.query(
+      "SELECT * FROM books WHERE id= ?",
+      [req.params.id],
+      (err, rows) => {
+        if (err) return res.send(err);
+
+        res.json(rows);
+      }
+    );
+  });
+});
+
 routes.post("/", (req, res) => {
   req.getConnection((err, conn) => {
     if (err) return res.send(err);
